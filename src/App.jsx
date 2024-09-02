0d6f5b39e67628b95af75642f8ece9ce15ea84c9
golu Kumar <golukumarjaisidih@gmail.com>
2024-09-03 00:59:57 +0530

    1st working version done

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import Grid from "./components/Grid/Grid"
 function App() {
   
   return (
-    <>
+    <div>
     <h1 className="header"> TIC-TAC-TOE&ensp;GAME  </h1>
       <Grid numberOfCards={9}/>
       
-    </>
+    </div>
   )
 }
 
