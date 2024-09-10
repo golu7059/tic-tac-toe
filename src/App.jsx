d9f15125d431bdde881930f3754fde7893382862
golu Kumar <golukumarjaisidih@gmail.com>
2024-09-10 23:53:08 +0530

    Added computer route

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Home from './pages/Home'
 import RoomPage from './pages/RoomPage';
 import Game from './pages/Game';
 import OfflinePlay from './pages/offlinePlay';
+import Computer from './pages/Computer';
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/offline" element={<OfflinePlay />} />
+        <Route path="/computer" element={<Computer />} />
         <Route path="/room" element={<RoomPage />} />
         <Route path="/game/:roomId" element={<Game />} />
       </Routes>
