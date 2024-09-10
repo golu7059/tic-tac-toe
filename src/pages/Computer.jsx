8aeb8156028210184dd56a38427604e970790216
golu Kumar <golukumarjaisidih@gmail.com>
2024-09-10 23:58:43 +0530

    updated computer mode

--- src/pages/Computer.jsx
+++ src/pages/Computer.jsx
@@ -1,11 +1,12 @@
-import Grid from '../components/Grid/Grid'
-function Computer(){
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-700">
-            <h1 className="text-2xl font-bold">Computer</h1>
-            <Grid numberOfCards={9}/>
-        </div>
-    )
+import Grid from '../components/Grid/Grid';
+
+function Computer() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-700">
+      <h1 className="text-4xl sm:text-5xl font-bold text-white mb-8">Play vs Computer</h1>
+      <Grid numberOfCards={9} />
+    </div>
+  );
 }
 
 export default Computer;
