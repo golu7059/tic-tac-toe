0bfdfc73e54805d0381ada95c8c1dc7a60858d56
golu Kumar <golukumarjaisidih@gmail.com>
2024-09-10 23:47:05 +0530

    offline page corrected

--- src/pages/offlinePlay.jsx
+++ src/pages/offlinePlay.jsx
@@ -1,12 +1,14 @@
 import Grid from "../components/Grid/Grid";
-function offlinePlay(){
 
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-            <h1 className="text-6xl font-bold">Offline Play</h1>
-            <Grid/>
-        </div>
-    )
-
+function OfflinePlay() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-[90%] bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 text-white p-4">
+      <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold tracking-wide mb-6 text-center">
+        Offline Play
+      </h1>
+        <Grid numberOfCards={9} />
+    </div>
+  );
 }
-export default offlinePlay;
+
+export default OfflinePlay;
