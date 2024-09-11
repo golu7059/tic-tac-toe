bbe6c72c30e677164a87adf542f74a4939885e00
golu Kumar <golukumarjaisidih@gmail.com>
2024-09-11 12:37:52 +0530

    added comments

--- src/helpers/checkWinner.js
+++ src/helpers/checkWinner.js
@@ -1,12 +1,15 @@
 function isWinner(board,symbol){
+    // check row wise
     if(board[0] == board[1] && board[1] == board[2] && board[2] == symbol) return symbol;
     if(board[3] == board[4] && board[4] == board[5] && board[5] == symbol) return symbol;
     if(board[6] == board[7] && board[7] == board[8] && board[8] == symbol) return symbol;
 
+    // check column wise
     if(board[0] == board[3] && board[3] == board[6] && board[6] == symbol) return symbol;
     if(board[1] == board[4] && board[4] == board[7] && board[7] == symbol) return symbol;
     if(board[2] == board[5] && board[5] == board[8] && board[8] == symbol) return symbol;
 
+    // check diagonal wise
     if(board[0] == board[4] && board[4] == board[8] && board[8] == symbol) return symbol;
     if(board[2] == board[4] && board[4] == board[6] && board[6] == symbol) return symbol;
 
